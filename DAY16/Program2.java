80e49509e5de420250a7aa330c1f88f613009efb
MrityunjaySingh09 <137044290+MrityunjaySingh09@users.noreply.github.com>
2025-05-18 00:41:04 +0530

    DAY16

---
+++ DAY16/Program2.java
@@ -0,0 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        if (node == null || node.next == null) return; // Edge case: last node can't be deleted this way
+        node.val = node.next.val; // Copy next node's value
+        node.next = node.next.next;
+    }
+}
